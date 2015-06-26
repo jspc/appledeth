7f765c87a3dee5526041f2fee5e47affc6695157
jspc <james@zero-internet.org.uk>
2015-06-26 17:12:56 +0100

    Colon to rocket

--- modules/appledeth/manifests/software.pp
+++ modules/appledeth/manifests/software.pp
@@ -2,7 +2,7 @@ class appledeth::software {
   # DM/ X/ Whatevs
   package { 'lightdm': ensure => 'present'}
   package { 'xorg-server-xephyr': ensure => 'present'}
-  service { 'lightdm.service': enable: true}
+  service { 'lightdm.service': enable => true}
 
   # Fluxbox
   package { 'fluxbox': ensure => 'present'}
